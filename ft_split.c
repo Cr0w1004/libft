507520ee78fc682faffae2d18ca15edc2c2d26f5
Alessandro Orru <alorru@c2r5p10.42firenze.it>
2024-01-11 14:32:48 +0100

    jazz

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: alorru <alorru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:38:16 by alorru            #+#    #+#             */
-/*   Updated: 2024/01/11 11:36:39 by alorru           ###   ########.fr       */
+/*   Updated: 2024/01/11 14:32:23 by alorru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,13 +59,38 @@ static char	**freemalloc(char *str)
 	return (NULL);
 }
 
-static char **ft_procedure(char **str, int *i, char *s, char oc)
+int	is_c(char check, char c)
 {
-	
+	if (check == c || !check == '\0' )
+		return (1);
+	return (0);
 }
 
-
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	
+	size_t	i;
+	size_t	j;
+	int		start;
+	char	**split;
+
+	split = ft_calloc((count(s, c) + 1), sizeof(char *));
+	if (!s || !split)
+		return (NULL);
+	i = 0;
+	j = 0;
+	start = 0;
+	while (i <= ft_strlen(s))
+	{
+		if ((is_c(s[i], c) && !is_c(s[i + 1], c)))
+			start = i + 1;
+		else if (!is_c(s[i], c) && is_c(s[i + 1], c))
+		{
+			split[j++] = ft_substr(s, start, i + 1 - start);
+			if (!split[j - i])
+				return (freemalloc(split, count(s, c)));
+		}
+		i++;
+	}
+	split[j] = NULL;
+	return (split);
 }
